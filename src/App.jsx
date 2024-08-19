8995a3d617d2e7c2e4325a6b548f2c9da1d64dea
gyozaa01 <olivia110321@gmail.com>
2024-08-20 03:53:28 +0900

    feat: 마커 원숭이로 변경, search와 list 연결하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Start from './Pages/Start';
 import Login from './Pages/Login';
@@ -11,6 +11,12 @@ import Search from './Pages/Search';
 import List from './Pages/List';
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+
+  const addPlace = (place) => {
+    setPlaces(prevPlaces => [...prevPlaces, place]);
+  };
+
   return (
     <Router>
       <Routes>
@@ -19,11 +25,10 @@ const App = () => {
         <Route path="/find-id" element={<FindId />} />
         <Route path="/send-temp-password" element={<SendTempPassword />} />
         <Route path="/signup" element={<Signup />} />
-
         <Route path="/select" element={<SelectMode />} />
         <Route path="/creator" element={<CreatorMode />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/list" element={<List />} />
+        <Route path="/search" element={<Search addPlace={addPlace} />} />
+        <Route path="/list" element={<List places={places} />} />
       </Routes>
     </Router>
   );
