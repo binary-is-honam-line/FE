03792aff14e904552e18485dbb613dc68bf8bf65
gyozaa01 <olivia110321@gmail.com>
2024-08-26 02:17:49 +0900

    feat: 도메인명 변경

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -1,7 +1,21 @@
 import axios from 'axios';
 
+// // 환경에 따라 baseURL 설정
+// const baseURL = 'http://localhost:8080';
+
+// const api = axios.create({
+//   baseURL,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   withCredentials: true, // 세션 쿠키를 포함하기 위해 설정
+// });
+
+// export default api;
+
+
 // 환경에 따라 baseURL 설정
-const baseURL = process.env.REACT_APP_API_BASE_URL;
+const baseURL = 'https://ukkikki.xyz';
 
 const api = axios.create({
   baseURL,
