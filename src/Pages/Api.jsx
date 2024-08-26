17c8ced563a1a8973df13302707f8094b1643140
gyozaa01 <olivia110321@gmail.com>
2024-08-26 21:55:16 +0900

    feat: api에 www추가

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ukkikki.xyz', // 상대 경로로 설정
+  baseURL: 'https://www.ukkikki.xyz', // 상대 경로로 설정
   headers: {
     'Content-Type': 'application/json',
   },
