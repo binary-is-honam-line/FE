9a03e52737c17258abfac305c21bc24e26e456ae
gyozaa01 <olivia110321@gmail.com>
2024-08-26 14:24:24 +0900

    feat: 프록시 재설정

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -1,21 +1,8 @@
 import axios from 'axios';
 
-// // 환경에 따라 baseURL 설정
-// const baseURL = 'http://localhost:8080';
-
-// const api = axios.create({
-//   baseURL,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   withCredentials: true, // 세션 쿠키를 포함하기 위해 설정
-// });
-
-// export default api;
-
-
-// 환경에 따라 baseURL 설정
-const baseURL = 'https://ukkikki.xyz';
+// 기본 baseURL 설정
+const PROXY = '/proxy'; // 프록시 경로 사용
+const baseURL = `${PROXY}/api`; // 프록시와 함께 API 경로 설정
 
 const api = axios.create({
   baseURL,
